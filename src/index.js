bb60ea383fa22c50ca45f624b33fb35650d127d9
Aadil Ahmed <aadilahmed0@gmail.com>
2023-04-29 04:51:27 -0700

    completed advanced deliverables

--- src/index.js
+++ src/index.js
@@ -12,10 +12,40 @@ document.addEventListener("DOMContentLoaded", () => {
         createRamen(ramenObj);
     });
 
+    document.querySelector("#edit-ramen").addEventListener('submit' , (e) => {
+        e.preventDefault();
+        const rating = e.target.querySelector('#new-rating').value;
+        const comment = e.target.querySelector('#new-comment').value;
+
+        fetch(`http://localhost:3000/ramens/${e.target.dataset.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify({
+                "rating": rating,
+                "comment": comment
+            })
+        })
+        .then((response) => response.json())
+        .then((ramenData) => { 
+            document.querySelector('#rating-display').innerText = ramenData.rating;
+            document.querySelector('#comment-display').innerText = ramenData.comment;
+        });
+    });
+
     fetch("http://localhost:3000/ramens")
     .then((response) => response.json())
     .then((ramenData) => {
-      ramenData.forEach(ramen => renderRamen(ramen));
+      document.querySelector('.detail-image').src = ramenData[0].image;
+      document.querySelector('.name').innerText = ramenData[0].name;
+      document.querySelector('.restaurant').innerText = ramenData[0].restaurant;
+      document.querySelector('#rating-display').innerText = ramenData[0].rating;
+      document.querySelector('#comment-display').innerText = ramenData[0].comment;
+      ramenData.forEach(ramen => {
+        renderRamen(ramen)
+    });
     });
 });
 
@@ -25,11 +55,16 @@ function renderRamen(ramen) {
     img.src = ramen.image;
 
     img.addEventListener('click', (e) => {
-        document.querySelector('.detail-image').src = ramen.image;
-        document.querySelector('.name').innerText = ramen.name;
-        document.querySelector('.restaurant').innerText = ramen.restaurant;
-        document.querySelector('#rating-display').innerText = ramen.rating;
-        document.querySelector('#comment-display').innerText = ramen.comment;
+        fetch(`http://localhost:3000/ramens/${ramen.id}`)
+        .then(response => response.json())
+        .then(ramen => {
+            document.querySelector('.detail-image').src = ramen.image;
+            document.querySelector('.name').innerText = ramen.name;
+            document.querySelector('.restaurant').innerText = ramen.restaurant;
+            document.querySelector('#rating-display').innerText = ramen.rating;
+            document.querySelector('#comment-display').innerText = ramen.comment;
+            document.querySelector("#edit-ramen").dataset.id = ramen.id;
+        })
     })
 
     document.querySelector("#ramen-menu").appendChild(img);
